b2d1a63bcaad3b574652fe39ae01456a2cae8c8d
Claire Jasmin Valdevieso <163674153+clairebear12@users.noreply.github.com>
2024-03-16 10:51:26 -0700

    Add files via upload

---
+++ flashcard/js/app.js
@@ -0,0 +1,28 @@
+const term = document.querySelector('.term');
+const definition = document.querySelector('.definition');
+const checkButton = document.querySelector('.check');
+const nextButton = document.querySelector('.next');
+
+words = {
+  "vol-": "to wish; to will; to want",
+  "bene-": "kindess; goodness",
+  "mal-": "both; changing",
+};
+
+data = Object.entries(words);
+
+function getRandomWord() {
+  randomTerm = data[Math.floor(Math.random() * data.length)];
+  term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
+  definition.innerHTML = `<h3>${randomTerm[1]}</h3>`;
+;}
+
+checkButton.addEventListener('click', function(){
+  definition.style.display = 'block';
+});
+
+nextButton.addEventListener('click', function() {
+  getRandomWord();
+})
+
+getRandomWord();
